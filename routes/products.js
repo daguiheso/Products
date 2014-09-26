4efb4c4339f527957457c2486bfdf2848eab9e96
Daniel <danielhernandez16@hotmail.com>
2014-09-25 21:58:43 -0700

    Create Links and view of  detalles del product

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var product = require('../lib/product');
+var Product = require('../lib/product');
 
 /* GET list Products */
 router.get('/', function(req, res) {
-  product.find({}, function(err, products){
+  Product.find({}, function(err, products){
   	if (err) {
   		return res.send(err)
   	}
-  	res.render('product/index',{products: products}); // objeto con nuestras variables
+  	res.render('products/index',{products: products}); // objeto con nuestras variables
+  });
+});
+
+router.get('/:product_id', function(req, res){
+  Product.findById(req.params.product_id, function(err, product){
+    res.render('products/detail', {product: product});
   });
 });
 
