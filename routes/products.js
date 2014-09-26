cb6c645336a2426b2c339e3deb2f507504501b38
Daniel <danielhernandez16@hotmail.com>
2014-09-26 06:05:54 -0700

    Create and configure route&views of Add Product

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../lib/product');
+var debug = require('debug')('my/application:products');//debug usado para saber cuando alguien esta en esta ruta / logs por conveniencia
+var _ = require('lodash');
 
 /* GET list Products */
 router.get('/', function(req, res) {
@@ -8,8 +10,13 @@ router.get('/', function(req, res) {
   	if (err) {
   		return res.send(err)
   	}
-  	res.render('products/index',{products: products}); // objeto con nuestras variables
+    res.render('products/index',{products: products}); // objeto con nuestras variables
+  	//res.send(products);  //formato JSON
   });
+});
+
+router.get('/add', function(req, res){
+	res.render('products/add');
 });
 
 router.get('/:product_id', function(req, res){
@@ -17,5 +24,25 @@ router.get('/:product_id', function(req, res){
     res.render('products/detail', {product: product});
   });
 });
+
+
+
+router.post('/', function(req, res){
+	debug('Creating new Product');  //mensaje en pantalla
+	var name = req.body.name;  // el nombre del product va a venir como un parametro del body
+
+	if(!_.isUndefined(name) || name !==''){  // si el product esta definido vamos a crear
+		var product = new Product({name:name});
+		product.save(function(err, product){
+			if (err) {
+				return res.send(err);
+			}
+			res.send(product.name + ' was saved');
+		});
+	}else{
+		res.send('Please specify product name');
+	}
+});
+
 
 module.exports = router;
